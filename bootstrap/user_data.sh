23bfbacbf7445ebf51f6a267164b9183a8ba0d41
Yu Chen <ychennay@gmail.com>
2019-08-03 21:06:24 -0700

    first working prototype

--- bootstrap/user_data.sh
+++ bootstrap/user_data.sh
@@ -10,7 +10,7 @@ curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.13
 openssl sha1 -sha256 aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$HOME/bin:$PATH
-echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=$HOME/bin:$PATH' >>~/.bashrc
 
 export KUBEFLOW_SRC=/tmp/kubeflow-aws
 export KUBEFLOW_TAG=v0.5-branch
@@ -23,15 +23,15 @@ export AWS_CLUSTER_NAME=kubeflow-aws
 
 export KS_VER=0.13.1
 export KS_PKG=ks_${KS_VER}_linux_amd64
-wget -O /tmp/${KS_PKG}.tar.gz https://github.com/ksonnet/ksonnet/releases/download/v${KS_VER}/${KS_PKG}.tar.gz   --no-check-certificate
+wget -O /tmp/${KS_PKG}.tar.gz https://github.com/ksonnet/ksonnet/releases/download/v${KS_VER}/${KS_PKG}.tar.gz --no-check-certificate
 tar -xvf /tmp/$KS_PKG.tar.gz -C ${HOME}/bin
 export PATH=$PATH:${HOME}/bin/$KS_PKG
-echo 'export PATH=$PATH:${HOME}/bin/$KS_PKG' > ~/.bash_rc
+echo 'export PATH=$PATH:${HOME}/bin/$KS_PKG' >~/.bash_rc
 
 # initialize and generate templates for platform config
 ${KUBEFLOW_SRC}/scripts/kfctl.sh init ${KFAPP} --platform aws \
---awsClusterName ${AWS_CLUSTER_NAME} \
---awsRegion ${REGION}
+  --awsClusterName ${AWS_CLUSTER_NAME} \
+  --awsRegion ${REGION}
 
 # apply configurations
 cd ${KFAPP}
@@ -42,3 +42,6 @@ ${KUBEFLOW_SRC}/scripts/kfctl.sh apply platform
 ${KUBEFLOW_SRC}/scripts/kfctl.sh generate k8s
 
 ${KUBEFLOW_SRC}/scripts/kfctl.sh apply k8s
+
+# install python aws api
+pip3 install boto3 && pip3 install --upgrade requests
--- notes.sh
+++ notes.sh
@@ -5,3 +5,5 @@ EXPORT EKS_CLUSTER_STACK=eksctl-kubeflow-aws-cluster
 aws cloudformation delete-stack --stack-name eksctl-kubeflow-aws-nodegroup-cpu-nodegroup --region=us-east-1
 aws cloudformation wait stack-delete-complete --stack-name eksctl-kubeflow-aws-nodegroup-cpu-nodegroup --region=us-east-1
 aws cloudformation delete-stack --stack-name eksctl-kubeflow-aws-cluster --region=us-east-1
+
+pip3 install boto3 && pip3 install --upgrade requests
