0b45a64e43293ddec13f24479e5a0825dd9441a8
Yu Chen <ychennay@gmail.com>
2019-08-11 16:05:17 -0700

    add .netrc shell script for databricks API token

---
+++ databricks/generate_netrc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Generating Databricks API netrc configuration"
+cat <<EOT >> $HOME/.netrc
+machine $DATABRICKS_ACCOUNT
+login token
+password $DATABRICKS_TOKEN
+EOT
+
+echo ".netrc file generated and stored in ${HOME}/.netrc"
