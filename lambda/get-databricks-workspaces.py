50216cdc97fc7f7fc5e7c0806a3874ea5080e19d
Yu Chen <ychennay@gmail.com>
2019-09-11 12:05:54 -0700

    Update get-databricks-workspaces.py

--- lambda/get-databricks-workspaces.py
+++ lambda/get-databricks-workspaces.py
@@ -1,9 +1,6 @@
-import json
-from botocore.vendored import requests 
-import os
-import boto3
 
-DATABRICKS_ACCOUNT = "dbc-e3636760-d7f1.cloud.databricks.com"
+from botocore.vendored import requests
+import boto3
 
 def make_url(account: str, resource: str, action: str) -> str:
     return f"https://{account}/api/2.0/{resource}/{action}"
@@ -20,28 +17,28 @@ method_dictionary = {
     "POST": "create"
 }
 
+
 def lambda_handler(event, context):
-    
     # Create Parameter Store client to access sensitive parameters (databricks token), give it the ability to decrypt
     ssm = boto3.client('ssm')
     DB_TOKEN = ssm.get_parameter(Name="databricks_token", WithDecryption=True)["Parameter"]["Value"]
     DATABRICKS_ACCOUNT = ssm.get_parameter(Name="databricks_deployment_hostname")["Parameter"]["Value"]
-    
+
     headers = {
-        "Authorization": f"Bearer {DB_TOKEN}" # attach the bearer token
+        "Authorization": f"Bearer {DB_TOKEN}"  # attach the bearer token
     }
     payload = {"path": "/Users/ychen244@syr.edu/"}
     action = method_dictionary[event["httpMethod"]]
     resource = event["path"].replace("/", "")
     final_resource = resource_dictionary[resource]
-    
+
     full_url = make_url(DATABRICKS_ACCOUNT, final_resource, action)
     response = requests.get(full_url, headers=headers, params=payload)
     return {
         "statusCode": 200,
         "isBase64Encoded": False,
         "headers": {
-            "Access-Control-Allow-Origin": "*" # enable CORS
+            "Access-Control-Allow-Origin": "*"  # enable CORS
         },
         "body": response.text
-        }
+    }
