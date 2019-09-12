cc50d0247381781e06cb1336a0fd1901861dff5d
Yu Chen <ychennay@gmail.com>
2019-09-12 12:16:45 -0700

    Update notes.sh

--- notes.sh
+++ notes.sh
@@ -1,5 +1,5 @@
-EXPORT NODEGROUP_STACK=eksctl-kubeflow-aws-nodegroup-cpu-nodegroup
-EXPORT EKS_CLUSTER_STACK=eksctl-kubeflow-aws-cluster
+export NODEGROUP_STACK=eksctl-kubeflow-aws-nodegroup-cpu-nodegroup
+export EKS_CLUSTER_STACK=eksctl-kubeflow-aws-cluster
 
 # delete dependent nodegroup stack
 aws cloudformation delete-stack --stack-name eksctl-kubeflow-aws-nodegroup-cpu-nodegroup --region=us-east-1
